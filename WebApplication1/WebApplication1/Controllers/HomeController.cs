a6f62a6a2531c65b63b8373334966685c5daa303
Volodymyr Melnyk <volodya842@gmail.com>
2018-05-08 21:50:20 +0300

    first version

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NodeContext db = new NodeContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<string> dataDB = new List<string>();
+
+            foreach (var item in db.Nodes.ToList())
+            {
+                dataDB.Add(Reverse(item.Sentence));
+            }
+            return View(dataDB);
+        }
+
+        public string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, string keyWord)
+        {
+            if (file != null)
+                try
+                {
+                    using (StreamReader read = new StreamReader(file.InputStream))
+                    {
+                        var line = read.ReadToEnd().Split('.');
+                        foreach (var item in line)
+                        {
+                            if (item.Contains(keyWord))
+                            {
+                                db.Nodes.Add(new Node(Reverse(item)));
+                                db.SaveChanges();
+                            }
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                }
+            else
+            {
+                ViewBag.Message = "Ви не вибрали файл!";
+            }
+
+            List<string> dataDB = new List<string>();
+
+            foreach (var item in db.Nodes.ToList())
+            {
+                dataDB.Add(Reverse(item.Sentence));
+            }
+            return View(dataDB);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Contexts/NodeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+
+namespace WebApplication1
+{
+    public class NodeContext:DbContext
+    {
+        public NodeContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Node> Nodes { get; set; }
+    }
+}
